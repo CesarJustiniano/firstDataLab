0e41a08508488c390fee469b22a8c318295abddf
ICOM STUDENTS <4020a6@nispero.ece.uprm.edu>
2018-02-23 13:59:48 -0400

    Revert "Added permiter feature"
    This reverts commit ae3a983e162ca1428f175a7a10a57bb71d3b18a6.
    
    "Fixing mistakes"

--- Main.java
+++ Main.java
@@ -5,13 +5,9 @@ public class AreaCircle{
 		Scanner scan = new Scanner(System.in);
 		double radius;
 		double area;
-
+		
 		System.out.print("Enter radius value: ");
 		radius = scan.nextDouble();
 		area = 2*Math.PI*radius*radius;
-
-		System.out.println("The area of half the circle is: " +
-			 area/2.0);
-		scan.close();
 	}
 }
